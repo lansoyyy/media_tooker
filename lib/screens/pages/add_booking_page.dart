6bdb03d8742967de98822fc3ea42ec2d2a3d4e9e
Lans12345 <olanalans12345@gmail.com>
2023-08-24 18:20:19 +0800

    wip add booking screen

---
+++ lib/screens/pages/add_booking_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddBookingPage extends StatelessWidget {
+  const AddBookingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(),
+    );
+  }
+}
