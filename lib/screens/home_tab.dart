b2f1b0c3fa2fa121bfcf4a118f0ffc7d6e79a65e
Lans12345 <olanalans12345@gmail.com>
2023-11-16 17:29:55 +0800

    added add portfolio func

--- lib/screens/home_tab.dart
+++ lib/screens/home_tab.dart
@@ -1,11 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:media_tooker/screens/pages/freelancers/bookings_page.dart';
 import 'package:media_tooker/screens/pages/messages_page.dart';
 import 'package:media_tooker/screens/pages/notif_page.dart';
 import 'package:media_tooker/screens/pages/profile_page.dart';
-
+import 'package:media_tooker/utils/colors.dart';
+import 'package:media_tooker/widgets/text_widget.dart';
+import 'package:media_tooker/widgets/toast_widget.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:path/path.dart' as path;
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 import 'home_screen.dart';
 
 class HomeTab extends StatefulWidget {
@@ -26,23 +35,168 @@ class _HomeTabState extends State<HomeTab> {
 
   final box = GetStorage();
 
+  late String fileName = '';
+
+  late File imageFile;
+
+  late String imageURL = '';
+
+  String selected = '';
+
+  Future<void> uploadPicture(
+      String inputSource, String selected, bool isVideo) async {
+    final picker = ImagePicker();
+    XFile pickedImage;
+    try {
+      pickedImage = isVideo
+          ? (await picker.pickVideo(
+              source: inputSource == 'camera'
+                  ? ImageSource.camera
+                  : ImageSource.gallery,
+            ))!
+          : (await picker.pickImage(
+              source: inputSource == 'camera'
+                  ? ImageSource.camera
+                  : ImageSource.gallery,
+              maxWidth: 1920))!;
+
+      fileName = path.basename(pickedImage.path);
+      imageFile = File(pickedImage.path);
+
+      try {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) => const Padding(
+            padding: EdgeInsets.only(left: 30, right: 30),
+            child: AlertDialog(
+                title: Row(
+              children: [
+                CircularProgressIndicator(
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  'Loading . . .',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'QRegular'),
+                ),
+              ],
+            )),
+          ),
+        );
+
+        await firebase_storage.FirebaseStorage.instance
+            .ref('Users/$fileName')
+            .putFile(imageFile);
+        imageURL = await firebase_storage.FirebaseStorage.instance
+            .ref('Users/$fileName')
+            .getDownloadURL();
+
+        await FirebaseFirestore.instance
+            .collection('Users')
+            .doc(FirebaseAuth.instance.currentUser!.uid)
+            .update({
+          'portfolio': FieldValue.arrayUnion([
+            {'img': imageURL, 'type': selected}
+          ])
+        });
+
+        showToast(isVideo
+            ? 'Video added to portfolio!'
+            : 'Image added to portfolio!');
+
+        Navigator.of(context).pop();
+      } on firebase_storage.FirebaseException catch (error) {
+        if (kDebugMode) {
+          print(error);
+        }
+      }
+    } catch (err) {
+      if (kDebugMode) {
+        print(err);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final List<Widget> children = box.read('user') == 'Client'
-        ? [
-            const HomeScreen(),
-            const MessagesPage(),
-            const NotifPage(),
-            ProfilePage(id: FirebaseAuth.instance.currentUser!.uid)
-          ]
-        : [
-            const HomeScreen(),
-            const MessagesPage(),
-            const BookingsPage(),
-            const NotifPage(),
-            ProfilePage(id: FirebaseAuth.instance.currentUser!.uid)
-          ];
+    final Stream<DocumentSnapshot> userData = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .snapshots();
+    final List<Widget> children = [
+      const HomeScreen(),
+      const MessagesPage(),
+      const NotifPage(),
+      ProfilePage(id: FirebaseAuth.instance.currentUser!.uid)
+    ];
     return Scaffold(
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterDocked,
+      floatingActionButton: FloatingActionButton(
+        mini: true,
+        backgroundColor: primary,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: StreamBuilder<DocumentSnapshot>(
+                    stream: userData,
+                    builder:
+                        (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+                      if (!snapshot.hasData) {
+                        return const SizedBox();
+                      } else if (snapshot.hasError) {
+                        return const Center(
+                            child: Text('Something went wrong'));
+                      } else if (snapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return const SizedBox();
+                      }
+                      dynamic data = snapshot.data;
+                      return SingleChildScrollView(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            for (int i = 0; i < data['job'].length; i++)
+                              ListTile(
+                                onTap: () {
+                                  Navigator.pop(context);
+                                  uploadPicture(
+                                      'gallery',
+                                      data['job'][i],
+                                      data['job'][i] == 'Videographer' ||
+                                          data['job'][i] == 'Editor' ||
+                                          data['job'][i] == 'Cinematographer' ||
+                                          data['job'][i] == 'Writer' ||
+                                          data['job'][i] == 'Director');
+                                },
+                                leading: const Icon(Icons.add),
+                                title: TextWidget(
+                                  text: data['job'][i],
+                                  fontSize: 14,
+                                  color: Colors.black,
+                                ),
+                              ),
+                          ],
+                        ),
+                      );
+                    }),
+              );
+            },
+          );
+        },
+      ),
       body: children[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         selectedLabelStyle: const TextStyle(fontFamily: 'QBold'),
@@ -83,10 +237,6 @@ class _HomeTabState extends State<HomeTab> {
                 const BottomNavigationBarItem(
                   icon: Icon(Icons.message),
                   label: 'Messages',
-                ),
-                const BottomNavigationBarItem(
-                  icon: Icon(Icons.add_circle_outline_outlined),
-                  label: 'Bookings',
                 ),
                 const BottomNavigationBarItem(
                   icon: Icon(Icons.notifications),
